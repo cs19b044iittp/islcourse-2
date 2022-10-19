eec816e95c90f29d2be2fee81f9776cdd15683c4
AMITKESARI2000 <kesari.amit33@gmail.com>
2022-10-19 11:48:45 +0530

    add sizex image train rename

--- hubconf.py
+++ hubconf.py
@@ -54,7 +54,7 @@ def get_model(train_data_loader=None, n_epochs=10):
   # Use softmax and cross entropy loss functions
   # set model variable to proper object, make use of train_data
   # with torch.no_grad():
-  for X, y in test_data_loader:
+  for X, y in train_data_loader:
       X, y = X.to(device), y.to(device)
       print("X and y shape", X.shape, y.shape)
       m=X.shape[0]
